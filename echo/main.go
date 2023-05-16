765d04ab6926204307f19f689bbf2cf40086b0dd
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-05-16 23:23:56 +0700

    Add files via upload

---
+++ echo/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type M map[string]interface{}
+
+var AcrionIndex = func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("from action index"))
+}
+
+var ActionHome = http.HandlerFunc(
+	func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("from action home"))
+	},
+)
+
+var ActionAbout = echo.WrapHandler(
+	http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("from action about"))
+		},
+	),
+)
+
+func main() {
+	r := echo.New()
+	r.Static("/static", "assets")
+
+	r.GET("/index", echo.WrapHandler(http.HandlerFunc(AcrionIndex)))
+	r.GET("/home", echo.WrapHandler(http.HandlerFunc(ActionHome)))
+	r.GET("/about", ActionAbout)
+
+	r.Start(":9000")
+}
