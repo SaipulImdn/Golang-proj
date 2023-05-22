fad8d7fa078171c08ae0504dcf4e567d83e38a2f
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-05-22 18:53:28 +0700

    Add files via upload

---
+++ 3rd-party/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
+)
+
+func makeLogEntry(c echo.Context) *log.Entry {
+	if c == nil {
+		return log.WithFields(log.Fields{
+			"at": time.Now().Format("2023-05-19 11:36:00"),
+		})
+	}
+
+	return log.WithFields(log.Fields{
+		"at":     time.Now().Format("2023-05-19 11:36:00"),
+		"method": c.Request().Method,
+		"uri":    c.Request().URL.String(),
+		"ip":     c.Request().RemoteAddr,
+	})
+}
+
+func middlewareLogging(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		makeLogEntry(c).Info("incoming request")
+		return next(c)
+	}
+}
+
+func errorHandler(err error, c echo.Context) {
+	report, ok := err.(*echo.HTTPError)
+	if ok {
+		report.Message = fmt.Sprintf("http error %d - %v", report.Code, report.Message)
+	} else {
+		report = echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	makeLogEntry(c).Error(report.Message)
+	c.HTML(report.Code, report.Message.(string))
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middlewareLogging)
+	e.HTTPErrorHandler = errorHandler
+
+	e.GET("/index", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, true)
+	})
+
+	lock := make(chan error)
+	go func(lock chan error) { lock <- e.Start(":9000") }(lock)
+	time.Sleep(1 * time.Millisecond)
+	makeLogEntry(nil).Warning("application started without ssl/tls enabled")
+	err := <-lock
+	if err != nil {
+		makeLogEntry(nil).Panic("failed to start application")
+	}
+}
